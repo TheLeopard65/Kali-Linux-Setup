cf58e6effac0858d8ae6bcb7d4837566d88fb2f9
TheLeopard65 <Leopardplaysctf@gmail.com>
2025-08-23 13:00:31 +0500

    A Small fix

--- get-tools.sh
+++ get-tools.sh
@@ -25,15 +25,15 @@ linux_scripts() {
     wget -q --show-progress https://raw.githubusercontent.com/redcode-labs/Bashark/refs/heads/master/bashark.sh -O bashark.sh && chmod +x bashark.sh
     wget -q --show-progress https://raw.githubusercontent.com/Pwnistry/Windows-Exploit-Suggester-python3/refs/heads/master/windows-exploit-suggester.py -O windows-exploit-suggester.py && chmod +x ./windows-exploit-suggester.py
     wget -q --show-progress https://raw.githubusercontent.com/sosdave/KeyTabExtract/refs/heads/master/keytabextract.py -O keytabextract.py && chmod +x keytabextract.py
-    wget -q --show-progress https://raw.githubusercontent.com/enjoiz/XXEinjector/refs/heads/master/XXEinjector.rb && chmod +x XXEinjector.rb && ln -s {$DIR}/XXEinjector.rb /usr/local/bin/XXEinjector
+    wget -q --show-progress https://raw.githubusercontent.com/enjoiz/XXEinjector/refs/heads/master/XXEinjector.rb && chmod +x XXEinjector.rb && ln -s {$DIR}/linux-scripts/XXEinjector.rb /usr/local/bin/XXEinjector
     wget -q --show-progress https://raw.githubusercontent.com/mzet-/linux-exploit-suggester/master/linux-exploit-suggester.sh -O linux-exploit-suggester.sh && chmod +x linux-exploit-suggester.sh
     wget -q --show-progress https://raw.githubusercontent.com/jondonas/linux-exploit-suggester-2/master/linux-exploit-suggester-2.pl -O linux-exploit-suggester-2.pl && chmod +x linux-exploit-suggester-2.pl
-    wget -q --show-progress https://raw.githubusercontent.com/urbanadventurer/username-anarchy/refs/heads/master/username-anarchy && chmod +x username-anarchy && ln -s {$DIR}/username-anarchy /usr/local/bin/username-anarchy
+    wget -q --show-progress https://raw.githubusercontent.com/urbanadventurer/username-anarchy/refs/heads/master/username-anarchy && chmod +x username-anarchy && ln -s {$DIR}/linux-scripts/username-anarchy /usr/local/bin/username-anarchy
     wget -q --show-progress https://github.com/DominicBreuker/pspy/releases/latest/download/pspy64 -O pspy64 && chmod +x pspy64
     wget -q --show-progress https://github.com/carlospolop/PEASS-ng/releases/latest/download/linpeas.sh -O linpeas.sh && chmod +x linpeas.sh
-    wget -q --show-progress https://github.com/assetnote/kiterunner/releases/download/v1.0.2/kiterunner_1.0.2_linux_amd64.tar.gz -O kiterunner-1.0.2.tar.gz && tar -xzf kiterunner-1.0.2.tar.gz && ln -s {$DIR}/kr /usr/local/bin/kr && rm kiterunner-1.0.2.tar.gz
+    wget -q --show-progress https://github.com/assetnote/kiterunner/releases/download/v1.0.2/kiterunner_1.0.2_linux_amd64.tar.gz -O kiterunner-1.0.2.tar.gz && tar -xzf kiterunner-1.0.2.tar.gz && ln -s {$DIR}/linux-scripts/kr /usr/local/bin/kr && rm kiterunner-1.0.2.tar.gz
     wget -q --show-progress https://github.com/jpillora/chisel/releases/download/v1.10.1/chisel_1.10.1_linux_amd64.gz -O chisel-linux-1.10.1.gz && gunzip chisel-linux-1.10.1.gz
-    wget -q --show-progress https://github.com/ropnop/kerbrute/releases/download/v1.0.3/kerbrute_linux_amd64 -O kerbrute && chmod +x ./kerbrute && ln -s {$DIR}/kerbrute /usr/local/bin/kerbrute
+    wget -q --show-progress https://github.com/ropnop/kerbrute/releases/download/v1.0.3/kerbrute_linux_amd64 -O kerbrute && chmod +x ./kerbrute && ln -s {$DIR}/linux-scripts/kerbrute /usr/local/bin/kerbrute
     wget -q --show-progress https://github.com/nicocha30/ligolo-ng/releases/download/v0.8.2/ligolo-ng_proxy_0.8.2_linux_amd64.tar.gz -O ligolo-proxy.tar.gz && tar -xzf ligolo-proxy.tar.gz && rm -rf LICENSE README.md ligolo-proxy.tar.gz && mv proxy ligolo-proxy
     wget -q --show-progress https://github.com/huntergregal/mimipenguin/releases/download/2.0-release/mimipenguin_2.0-release.tar.gz -O mimipenguin-2.0.tar.gz && tar -xzf mimipenguin-2.0.tar.gz && mv mimipenguin_2.0-release mimipenguin-2.0 && rm -f mimipenguin-2.0.tar.gz
     echo "Cloning the Linux Kernel Exploits Repository" && git clone --quiet https://github.com/JlSakuya/Linux-Privilege-Escalation-Exploits.git
