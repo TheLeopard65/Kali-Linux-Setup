9b0ef37f11d6b01ddb5e2f9928d365cd2a7fd3fd
TheLeopard65 <Yasirali752550@gmail.com>
2025-12-14 16:18:08 +0500

    Fixing Original Mistakes

--- pwn-setup.sh
+++ pwn-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Author: Leopard
+set -euo pipefail
 
 # Checking if the script is being executed as root or not.
 if [[ "$(id -u)" -ne 0 ]]; then
@@ -7,12 +8,18 @@ if [[ "$(id -u)" -ne 0 ]]; then
     exit 1
 fi
 
+if ! command -v figlet &> /dev/null; then
+  apt-get -qq install -y figlet
+fi
+
 # Some important configurations such colors and Helping Functions
-set -eo pipefail
-GREEN="\033[0;32m"
 YELLOW="\033[1;33m"
+GREEN="\033[0;32m"
+BLUE="\033[0;34m"
 RED="\033[0;31m"
 NC="\033[0m"
+
+info() { echo -e "${BLUE}[INFO]${NC} $1"; }
 error() { echo -e "${RED}[ERROR]${NC} $1"; }
 success() { echo -e "${GREEN}[ OK ]${NC} $1"; }
 
@@ -24,36 +31,37 @@ echo -e "${GREEN}[###] ---------------------------------------------------------
 export DEBIAN_FRONTEND=noninteractive
 echo '* libraries/restart-without-asking boolean true' | debconf-set-selections
 
-if ! command -v figlet &> /dev/null; then
-  apt-get -qq install -y figlet
-fi
-
 TARGET_USER=${SUDO_USER:-$(whoami)}
 TARGET_HOME=$(eval echo "~$TARGET_USER")
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
 # Performing Necessary Updates and Upgrades to the Target System
+info "[##] Updating and Upgrading the System Packages  --------------------- [ #1 ]"
 apt-get update -qq
 apt-get upgrade -y -qq
 apt-get full-upgrade -y -qq
 apt-get autoremove -y -qq
 
 # Installing the necessary requried packages for Binary Exploitation
+info "[##] Installing the Compulsory/Required Packages  -------------------- [ #2 ]"
 apt-get -qq install -y python3 python3-dev python3-venv python3-pip pipx gdb radare2 pwncat strace ltrace binutils python3-requests > /dev/null
 apt-get -qq install -y netcat-traditional ncat nmap python3-flask socat impacket-scripts plocate > /dev/null
 
 # Creating a new Python3 Virtual Environment in the Home Directory.
+info "[##] Creating a Python3 Environment & Installing Libraries  ---------- [ #3 ]"
 python3 -m venv "$TARGET_HOME/PWN-VENV"
 source "$TARGET_HOME/PWN-VENV/bin/activate"
-pip3 install pwntools pwn-flashlib ROPGadget one_gadget
+pip3 install pwntools pwn-flashlib ROPGadget one_gadget > /dev/null
 deactivate
 chown -R "$TARGET_USER:$TARGET_USER" "$TARGET_HOME/PWN-VENV"
 
 # Installing some necessary Python3 Cmdlets through the Pipx Command.
-pipx install pwntools ROPGadget one_gadget pwn-flashlib impacket --include-deps
-pipx --ensure-path
+info "[##] Creating tools and Cmdlets via PIPX  ---------------------------- [ #4 ]"
+pipx install pwntools ROPGadget one_gadget impacket --include-deps > /dev/null
+pipx ensurepath
 
 # Finall Update, Upgrade and Auto-remove.
+info "[##] Final Update and Upgrade to the System Packages  ---------------- [ #5 ]"
 apt-get -qq update
 apt-get -qq upgrade -y
 apt-get -qq full-upgrade -y
